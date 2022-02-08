d8f5f1f62d870bc94d78bfb9439be94ec641401c
Andrew Hong <35881688+NovialRiptide@users.noreply.github.com>
2022-02-08 08:32:17 -0500

    fixed importerror

--- src/data/scripts/entity.py
+++ src/data/scripts/entity.py
@@ -1,6 +1,6 @@
-from SakuyaEngine import BaseEntity
+from SakuyaEngine.entity import Entity
 
 
-class Entity(BaseEntity):
+class Entity(Entity):
     def __init__(self) -> None:
         super().__init__()
