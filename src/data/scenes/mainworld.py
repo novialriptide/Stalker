443cb1753ba8263d6f16c38bffe346710efa2143
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2022-02-08 01:48:04 -0500

    self.collisions -> self.light_collisions + added new camera + added light collisions

--- src/data/scenes/mainworld.py
+++ src/data/scenes/mainworld.py
@@ -1,6 +1,6 @@
 from SakuyaEngine.scene import Scene
 from SakuyaEngine.lights import LightRoom
-from SakuyaEngine.math import get_angle
+from SakuyaEngine.math import get_angle, rect_to_lines
 
 from data.scripts.map_loader import GameMap
 
@@ -9,12 +9,20 @@ from math import degrees
 import pygame
 import sys
 
+
 class MainWorld(Scene):
     def on_awake(self):
         self.lightroom = LightRoom(self)
-        self.collisions = [[pygame.Vector2(75, 35), pygame.Vector2(75, 75)]]
-        
-        self.g = GameMap("C:/Users/novia/Documents/GitHub/Stay-Alert/src/data/tilemaps/house.tmx")
+        self.light_collisions = []
+
+        self.g = GameMap(
+            "C:/Users/novia/Documents/GitHub/Stay-Alert/src/data/tilemaps/house.tmx"
+        )
+
+        for r in self.g.collision_rects:
+            line = rect_to_lines(r)
+            self.light_collisions.extend(line)
+            self.collision_rects.append(r)
 
     def update(self):
         for event in pygame.event.get():
@@ -24,19 +32,20 @@ class MainWorld(Scene):
 
         self.screen.fill((0, 0, 255))
 
-        self.lightroom.draw_point_light(screen_size / 2, 35, collisions=self.collisions)
-
-        dir = degrees(get_angle(self.client.mouse_pos, screen_size / 2)) + 180
-        self.lightroom.draw_spot_light(
-            screen_size / 2, 150, dir, 65, collisions=self.collisions
-        )
         dir = degrees(get_angle(self.client.mouse_pos, screen_size / 3)) + 180
         self.lightroom.draw_spot_light(
-            screen_size / 3, 150, dir, 45, collisions=self.collisions
+            screen_size / 3, 150, dir, 70, collisions=self.light_collisions
         )
-        self.screen.blit(self.g.surface, (0, 0))
-        
+        map_surf = self.g.surface
+        map_surf_size = self.g.size
+        self.screen.blit(map_surf, (0, 0))
         self.screen.blit(self.lightroom.surface, (0, 0))
 
-        for c in self.collisions:
+        for c in self.light_collisions:
             pygame.draw.line(self.screen, (255, 0, 0), c[0], c[1])
+
+        # Camera
+        self.camera.position = pygame.Vector2(
+            screen_size.x / 2 - map_surf_size.x / 2,
+            screen_size.y / 2 - map_surf_size.y / 2,
+        )
