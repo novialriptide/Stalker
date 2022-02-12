eaeda84dfa5790627c55bf8ff1c66ece9dd83d99
Andrew Hong <35881688+NovialRiptide@users.noreply.github.com>
2022-02-11 23:23:04 -0500

    Added KEYBOARD

---
+++ src/data/scripts/const.py
@@ -0,0 +1,18 @@
+import pygame
+
+KEYBOARD = {
+    "up1": pygame.K_w,
+    "left1": pygame.K_a,
+    "down1": pygame.K_s,
+    "right1": pygame.K_d,
+    "up2": pygame.K_w,
+    "left2": pygame.K_a,
+    "down2": pygame.K_s,
+    "right2": pygame.K_d,
+    "A": pygame.K_z,
+    "B": pygame.K_x,
+    "X": None,
+    "Y": None,
+    "select": pygame.K_q,
+    "start": pygame.K_ESCAPE,
+}
