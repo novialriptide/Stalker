966c997c41ff90eccd6041813bf496ed161b517d
Andrew Hong <35881688+NovialRiptide@users.noreply.github.com>
2022-02-11 23:22:20 -0500

    Added __init__ override

--- src/data/scripts/entity.py
+++ src/data/scripts/entity.py
@@ -1,6 +1,30 @@
 from SakuyaEngine.entity import Entity
 
+from typing import List
+
+import pygame
+
 
 class Entity(Entity):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(
+        self,
+        name: str = None,
+        tags: List[str] = [],
+        scale: float = 1,
+        position: pygame.Vector2 = pygame.Vector2(0, 0),
+        obey_gravity: bool = False,
+        speed: float = 0,
+        custom_hitbox_size: pygame.Vector2 = pygame.Vector2(0, 0),
+        gravity_scale: float = 1,
+    ) -> None:
+        super().__init__(
+            name=name,
+            tags=tags,
+            scale=scale,
+            position=position,
+            obey_gravity=obey_gravity,
+            speed=speed,
+            custom_hitbox_size=custom_hitbox_size,
+            gravity_scale=gravity_scale,
+        )
+        self.gravity = pygame.Vector2(0, 0)
--- src/data/scripts/player.py
+++ src/data/scripts/player.py
@@ -1,6 +1,32 @@
 from .entity import Entity
 
+from typing import List
+
+import pygame
+
 
 class Player(Entity):
+    def __init__(
+        self,
+        name: str = None,
+        tags: List[str] = [],
+        scale: float = 1,
+        position: pygame.Vector2 = pygame.Vector2(0, 0),
+        obey_gravity: bool = False,
+        speed: float = 0.5,
+        custom_hitbox_size: pygame.Vector2 = pygame.Vector2(0, 0),
+        gravity_scale: float = 0,
+    ) -> None:
+        super().__init__(
+            name=name,
+            tags=tags,
+            scale=scale,
+            position=position,
+            obey_gravity=obey_gravity,
+            speed=speed,
+            custom_hitbox_size=custom_hitbox_size,
+            gravity_scale=gravity_scale,
+        )
+
     def on_update(self) -> None:
         print("d")
