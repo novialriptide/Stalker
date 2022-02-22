1c8f66668366408cf862f03c2677b1b70b074c1c
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2022-02-22 00:28:46 -0500

    player moves back to original position when coming out of hiding

--- src/data/scripts/map_cmds.py
+++ src/data/scripts/map_cmds.py
@@ -7,6 +7,7 @@ def hide(**kwargs) -> None:
         rect.x + rect.width / 2 - player_rect.width / 2,
         rect.y + rect.height / 2 - player_rect.height / 2
     )
+    kwargs["player"].back_hide_pos = kwargs["player"].position
     kwargs["player"].hiding = True
 
 CMDS = {
