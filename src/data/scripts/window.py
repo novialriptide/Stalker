51f27a5c0f2281e9dc35136d7be27d7d6de4c7a7
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2022-02-22 17:10:19 -0500

    Added draw_light() + finished area_light_points

--- src/data/scripts/window.py
+++ src/data/scripts/window.py
@@ -1,3 +1,6 @@
+import pygame
+
+
 class Window:
     def __init__(
         self,
@@ -13,4 +16,37 @@ class Window:
 
     @property
     def area_light_points(self):
-        return None
+        possible_points = {
+            "up": [
+                pygame.Vector2(self.rect.x, self.rect.y),
+                pygame.Vector2(self.rect.x + self.rect.width, self.rect.y),
+            ],
+            "down": [
+                pygame.Vector2(self.rect.x, self.rect.y + self.rect.height),
+                pygame.Vector2(
+                    self.rect.x + self.rect.width, self.rect.y + self.rect.height
+                ),
+            ],
+            "left": [
+                pygame.Vector2(self.rect.x, self.rect.y),
+                pygame.Vector2(self.rect.x, self.rect.y + self.rect.height),
+            ],
+            "right": [
+                pygame.Vector2(self.rect.x + self.rect.width, self.rect.y),
+                pygame.Vector2(
+                    self.rect.x + self.rect.width, self.rect.y + self.rect.height
+                ),
+            ]
+            
+        }
+        
+        return possible_points[self.window_dir]
+
+    def draw_light(self, lightroom):
+        points = self.area_light_points
+        lightroom.draw_area_light(
+            points[0],
+            points[1],
+            self.light_length,
+            float(self.light_dir),
+        )
