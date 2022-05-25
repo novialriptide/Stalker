20fc83dd0efd623d5ae0345c6729a7ba1c0643a5
Andrew Hong <35881688+NovialRiptide@users.noreply.github.com>
2022-05-25 12:02:42 -0400

    Discord RPC implemented

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
+from data.scripts.const import DISCORD_RPC_CLIENT_ID
+
 import argparse
+import time
 import pygame
+import pypresence
 import SakuyaEngine as engine
 
 client = engine.Client(
@@ -22,9 +26,13 @@ for s in scenes:
 parser = argparse.ArgumentParser()
 parser.add_argument("--scene", type=str, help="Load a scene")
 parser.add_argument("--fps", type=int, help="Set the game fps")
+parser.add_argument(
+    "--rpc", type=bool, help="Enables/disables Discord RPC; enabled on default"
+)
 args = parser.parse_args()
 
 client.max_fps = 60
+
 
 if args.fps is not None:
     client.max_fps = args.fps
@@ -33,5 +41,13 @@ if args.scene is not None:
     client.add_scene(args.scene)
 else:
     client.add_scene("MainWorld")
+
+if args.rpc is None or args.rpc:
+    try:
+        rpc = pypresence.Presence(DISCORD_RPC_CLIENT_ID)
+        rpc.connect()
+        rpc.update(start=time.time())
+    except:
+        print("Something went wrong starting up Discord RPC.")
 
 client.main()
